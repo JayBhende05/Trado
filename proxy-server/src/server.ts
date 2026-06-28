7487f6a6781d3e63116a596a4d4dc718a9e2a80e
JayBhende05 <bhendejay5.bms@gmail.com>
2026-06-28 11:29:56 +0530

    Changed from proxy server to Binance data service

--- proxy-server/src/server.ts
+++ proxy-server/src/server.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-import {createProxyMiddleware} from 'http-proxy-middleware'
 import cors from 'cors'
 
 
@@ -10,16 +9,72 @@ const port = 3001;
 
 app.use(cors());
 
-app.use('/', createProxyMiddleware({
-	target: targetUrl,
-	changeOrigin: true,
-	  headers: {
-      "User-Agent": "Mozilla/5.0",
-      "Accept": "application/json",
-    },
-}));
+
+app.get("/ticker", async (req: Request, res: Response) => {
+  try {
+    const response = await fetch("https://api.binance.com/api/v3/ticker/price");
+
+    if (!response.ok) {
+      return res.status(response.status).json({
+        success: false,
+        message: "Failed to fetch data from Binance",
+      });
+    }
+
+    const data = await response.json();
+
+    return res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    console.error("Error fetching ticker:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+});
 
 
+app.get("/depth", async (req: Request, res: Response) => {
+  try {
+    const symbol = req.query.symbol as string;
+
+    if (!symbol) {
+      return res.status(400).json({
+        success: false,
+        message: "Symbol required",
+      });
+    }
+
+    const response = await fetch(
+      `https://api.binance.com/api/v3/depth?symbol=${symbol}&limit=100`
+    );
+
+    if (!response.ok) {
+      return res.status(response.status).json({
+        success: false,
+        message: "Failed to fetch data from Binance",
+      });
+    }
+
+    const data = await response.json();
+
+    return res.status(200).json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    console.error("Failed to fetch symbol depth data", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+});
 app.get('/health', (req: Request ,res: Response) => {
 
 	res.status(200).json({
